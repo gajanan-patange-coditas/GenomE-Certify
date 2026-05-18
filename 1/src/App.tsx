d83d3430a8c76a922c04a76bede6847280e8acaa
gajanan.patange-coditas <gajanan.patange@coditas.com>
2026-05-18 18:14:37 +0530

    intial commit

---
+++ 1/src/App.tsx
@@ -0,0 +1,9 @@
+
+export const App = () => {
+  
+  return(
+    <>
+    <h1>this is app</h1>
+    </>
+  )
+}
---
+++ 1/src/Components/Landing/Landing.tsx
@@ -0,0 +1,18 @@
+import styles from './Landing.module.scss'
+export  const Landing = () => {
+    return(
+        <>
+        <div className={styles.Landingpage}>
+            <header className={styles.header}>
+                
+            </header>
+            <main className={styles.main}>
+                
+
+            </main>
+            <footer className={styles.footer}></footer>
+        </div>
+        
+        </>
+    )
+}
